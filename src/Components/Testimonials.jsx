0659c1311f205f2007bbe74d2de025e624c507e6
mayank-49 <mayankkuma04@gmail.com>
2025-01-17 22:11:26 +0530

    Update Testimonials.jsx

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { testimonials } from '../constants/index'
 const Testimonials = () => {
   return (
-    <div className='mt-20 tracking-wide'>
+    <div id='testimonial' className='mt-20 tracking-wide'>
         <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20'>What people are saying</h2>
         <div className="flex flex-wrap justify-center ">
             {testimonials.map((testimonial, index)=>(
