d96a221fcad1034c4ebc54f7b408f06d983bd988
mayank-49 <mayankkuma04@gmail.com>
2025-01-17 22:11:07 +0530

    Update Pricing.jsx

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -3,7 +3,7 @@ import { CheckCircle2 } from 'lucide-react';
 import { pricingOptions } from '../constants/index';
 const Pricing = () => {
   return (
-    <div className='mt-20 '>
+    <div id='pricing' className='mt-20 '>
         <h2 className='text-3xl sm-text-5xl lg:text-6xl text-center my-8 tracking-wide'>Pricing</h2>
         <div className="flex flex-wrap">
             {pricingOptions.map((option, index)=>(
