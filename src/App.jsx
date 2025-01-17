6bb8ad176427dca59aa73e254e0f17324f128169
mayank-49 <mayankkuma04@gmail.com>
2025-01-17 19:01:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './App.css';
+import Navbar from "./Components/Navbar"
+import HeroSection from './Components/HeroSection';
+import FeatureSection from './Components/FeatureSection';
+import Workflow from './Components/workflow';
+import Pricing from './Components/Pricing';
+import Testimonials from './Components/Testimonials';
+import Footer from './Components/Footer';
+
+const App= ()=> {
+  return(
+    <>
+      <Navbar/>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+      <HeroSection/>  
+      <FeatureSection/>
+      <Workflow/>
+      <Pricing/>
+      <Testimonials/>
+      <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
